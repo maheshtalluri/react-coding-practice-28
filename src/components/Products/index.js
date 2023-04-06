a12438a330b37d0c31f7039f511f79966665c802
root <tallurimahesh169@gmail.com>
2023-04-06 15:59:02 +0000

    coding practice 28

---
+++ src/components/Products/index.js
@@ -0,0 +1,21 @@
+// Write your JS code here
+
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+
+    <div className="product-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="product-img-size"
+      />
+    </div>
+  </>
+)
+
+export default Products
